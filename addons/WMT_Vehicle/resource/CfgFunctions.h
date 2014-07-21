0d7c46e4b91980a3f092d2ce791460a7f61d7210
iEzhuk <a1dokuchaev@gmail.com>
2014-07-21 15:55:54 +0400

    Rebase code + icon for module

---
+++ addons/WMT_Vehicle/resource/CfgFunctions.h
@@ -0,0 +1,28 @@
+class CfgFunctions
+{
+	class WMT
+	{
+		class Vehicle
+		{
+			file="WMT_Vehicle\functions";		
+			class CheckKindOfArray{};
+			class FieldRepairVehicle{};
+			class FullRepair{};
+			class InitModuleVehicle{};
+			class KeyHandlerShowCrew{};
+			class LowGearCond{};
+			class LowGear{};
+			class NotifyText{};
+			class PartRepair{};
+			class PushBoat{};
+			class ReammoCond{};
+			class ReammoGetNearest{};
+			class Reammo{};
+			class RearmVehicle{};
+			class ShowCrew{};
+			class StaticWpnDrag{};
+			class VehicleIsDamaged{};
+	
+		};
+	};
+};
---
+++ addons/WMT_Vehicle/resource/CfgVehicles.h
@@ -0,0 +1,67 @@
+class ArgumentsBaseUnits;
+class CfgVehicles
+{
+	#include "CfgModules.h"
+
+	class B_Soldier_base_F;
+	class B_engineer_F : B_Soldier_base_F
+	{
+		engineer = 0;
+	};
+	class B_Story_Engineer_F : B_Soldier_base_F
+	{
+		engineer = 0;
+	};
+	class B_CTRG_soldier_engineer_exp_F : B_Soldier_base_F
+	{
+		engineer = 0;
+	};
+	class B_soldier_repair_F : B_Soldier_base_F
+	{
+		engineer=0;
+	};
+
+	class O_Soldier_base_F;
+	class O_Soldier_Urban_base;
+	class O_engineer_U_F : O_Soldier_base_F 
+	{
+		engineer = 0;
+	};
+	class O_engineer_F: O_Soldier_base_F
+	{
+		engineer = 0;
+	};
+
+	class O_soldier_repair_F : O_Soldier_base_F 
+	{
+		engineer = 0;
+	};
+	class O_soldierU_repair_F  : O_Soldier_Urban_base  
+	{
+		engineer=0;		
+	};
+
+	class I_Soldier_base_F;
+	class I_engineer_F : I_Soldier_base_F
+	{
+		engineer=0;
+	};
+	class I_soldier_repair_F : I_Soldier_base_F
+	{
+		engineer=0;
+	};
+
+	class I_G_Soldier_base_F;
+	class I_G_engineer_F : I_G_Soldier_base_F
+	{
+		engineer=0;
+	};
+	class B_G_engineer_F : I_G_engineer_F
+	{
+		engineer = 0;
+	};
+	class O_G_engineer_F   : I_G_engineer_F
+	{
+		engineer=0;
+	};	
+};
