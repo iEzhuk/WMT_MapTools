f1fba1cdd2c72a059e18497a6db45ae52b9bb15d
Zealot111 <Zealot111@users.noreply.github.com>
2015-01-27 13:42:12 +0300

    new version

--- addons/WMT_Main/config.cpp
+++ addons/WMT_Main/config.cpp
@@ -17,9 +17,9 @@ class CfgPatches
 		requiredAddons[] = {};
 		author[] = {"Ezhuk","Zealot"};
 		authorUrl = "https://github.com/iEzhuk/WMT_MapTools";
-		version = 1.4.1;
-		versionStr = "1.4.1";
-		versionAr[] = {1,4,1};
+		version = 1.4.2;
+		versionStr = "1.4.2";
+		versionAr[] = {1,4,2};
 	};
 };
 
